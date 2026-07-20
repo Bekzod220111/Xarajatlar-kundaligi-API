0a71cc3a5aba40c6fa2bf95dba85a4f550cd113d
Bekzod <tobby4173@gmail.com>
2026-07-20 10:42:58 +0500

    Refactor Expense model and serializer: update field types and validation logic

--- src/api/user/serializers/expense_serializers.py
+++ src/api/user/serializers/expense_serializers.py
@@ -1,5 +1,7 @@
 from rest_framework.serializers import ModelSerializer
 from apps.sarf.models import Expense
+from django.utils import timezone
+from rest_framework.validators import ValidationError
 
 class ExpenseListSerializer(ModelSerializer):
 
@@ -7,13 +9,8 @@ class ExpenseListSerializer(ModelSerializer):
         model = Expense
         fields = '__all__'
 
-    def validate_summa(self, value):
-            if value <= 0:
-                raise serializers.ValidationError('Xarajatingiz 0 som dan katta bolishi kerak')
-            return value
-
-
-    def validate_spent_date(self, value):
-        if value > timezone.now().date():
-            raise serializers.ValidationError('Xarajat sansi kelajakda bolishi mumkin emas')
-        return value
+    def validate_xarajat_sanasi(self, xarajat_sanasi):
+       now = timezone.now()
+       if xarajat_sanasi > now:
+          raise ValidationError("Xarajat kelajakda bolmaydi")
+       return xarajat_sanasi
--- src/apps/sarf/models.py
+++ src/apps/sarf/models.py
@@ -9,9 +9,9 @@ class Category(models.Model):
 
 class Expense(models.Model):
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
-    summa = models.DecimalField(max_digits=10, decimal_places=2)
+    summa = models.PositiveIntegerField()
     izoh = models.TextField(blank=True, null=True)
-    xarajat_sanasi = models.DateField(auto_now=True)
+    xarajat_sanasi = models.DateTimeField()
 
     def __str__(self):
         return self.summa
